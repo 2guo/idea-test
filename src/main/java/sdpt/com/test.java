8c54f282244293c990b68a499d0ac1acd38f3e6a
GSX <1335189971@qq.com>
2022-07-16 12:57:46 +0800

    master second

--- src/main/java/sdpt/com/test.java
+++ src/main/java/sdpt/com/test.java
@@ -6,7 +6,9 @@ public class test {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("远程库test");
+        System.out.println("本地库test");
     }
 }
